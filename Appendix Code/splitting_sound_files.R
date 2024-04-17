b6339d4812bb7d857c1aa8896375a885d9944444
carlaleone <146276297+carlaleone@users.noreply.github.com>
2024-04-17 08:22:36 +0100

    Update splitting_sound_files.R

--- Appendix Code/splitting_sound_files.R
+++ Appendix Code/splitting_sound_files.R
@@ -2,7 +2,6 @@
 # modification of code from Issy Key edited by Carla Leone
 # Output is 1-minute long recordings every 10 minutes
 
-## Load and install packages ---- 
 install.packages("soundecology")
 install.packages("seewave")
 install.packages("tuneR")
@@ -10,16 +9,15 @@ install.packages("tuneR")
 library(soundecology)
 library(tuneR)
 library(seewave)
+library(tools)
 
-## Set the working directory ----
 getwd()
-audiofiles = "C:/Users/frleo/OneDrive/Desktop/Index/Recordings" #this is the local working directory where all my files are stored
+audiofiles = "C:/Users/frleo/OneDrive - University of Edinburgh/Dissertation/Index/Leftover"
 setwd(audiofiles)
 
-## Create a list of the sound files
+
 wav_list <- list.files( pattern = ".WAV") #"." means the path defaults to the current working directory
 
-## Create a folder for the output of the split files
 output_folder <- file.path(audiofiles, "Split")
 dir.create(output_folder, showWarnings = TRUE) #if can't see the folder created already, repeat the action and it will say it already exists, if it worked the first time
 
@@ -33,7 +31,7 @@ for(i in seq_along(wav_list)) {
   
   #Downsample the audio if desired
   # # ***CHECK WITH EACH RUN***
-  audio <- downsample(audio,samp.rate=18000) #Experiment with different frequencies to downsample to - this decreases the time for the code to run
+  audio <- downsample(audio,samp.rate=40000) #Experiment with different frequencies to downsample to - this decreases the time for the code to run
   
   # Define the duration of each section in seconds
   section_duration <- 60
@@ -49,10 +47,10 @@ for(i in seq_along(wav_list)) {
   parent_folder_name <- basename(dirname(dirname(wav_list[i])))
   
   # Loop to cut the audio into 60-second sections
-  for (j in 1:num_sections) {
+  for (j in 0:num_sections) {
     
     #Only save every 5th cut audio file
-    if (j %% 20 == 0) { 
+    if (j %% 10 == 0) { 
       
       # Calculate the start and end times for the current section
       start_time <- (j - 1) * section_duration
@@ -61,21 +59,25 @@ for(i in seq_along(wav_list)) {
       # Cut the audio for the current section
       section_audio <- cutw(audio, from = start_time, to = end_time, output = "Wave")
       
-      # Create an output file name (caused problems before because there was a double .wav extension in the file name)
-      output_file_name <- file.path(output_folder, paste0(parent_folder_name, "_", basename(wav_list[i]), "_", j, ".WAV"))
+      # Create an output file name
+      output_file_name <- file.path(output_folder, paste0(tools::file_path_sans_ext(basename(wav_list[i])), j, ".WAV"))
       
       # Write the current section to a new WAV file
       writeWave(section_audio, output_file_name)
     }
   }
 }
+
+
 end <- Sys.time()
 duration <- end - start
 duration
+# 1.5 hours to split 11 files
 
-#in WINDOWS save the output into the outpur folder ----
+#or in WINDOWS:
 shell.exec(output_folder)
 
 #This worked and below is the file path
 
 #"C:\Users\frleo\OneDrive\Desktop\Index\Recordings\Split\._20220626_000004.WAV_20.WAV"
+
